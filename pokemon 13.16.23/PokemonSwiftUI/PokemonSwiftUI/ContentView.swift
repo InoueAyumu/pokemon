5e0eabdb85fa8fb30aed80239788bf69ae913bf7
inoue ayumu <>
2025-04-24 09:04:16 +0900

    画面遷移の変更

--- pokemon 13.16.23/PokemonSwiftUI/PokemonSwiftUI/ContentView.swift
+++ pokemon 13.16.23/PokemonSwiftUI/PokemonSwiftUI/ContentView.swift
@@ -10,14 +10,6 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var pokemonViewModel = PostViewModel()
-//    @Environment(\.managedObjectContext) private var viewContext
-//    @FetchRequest private var sessionList: FetchedResults<NSFetchRequestResult>
-//       
-//    init() {
-//        _sessionList = FetchRequest(
-//            sortDescriptors: []
-//        )
-//    }
     
     var body: some View {
         //ナビゲーションの設定
@@ -46,55 +38,78 @@ struct ContentView: View {
                     LazyVGrid(columns: columns, spacing: 20) {
                         //同じ作業の繰り返し（ポケモンViewのポストポケの順番通りに一番から）
                         ForEach(pokemonViewModel.postPoke.sorted(by: { $0.id < $1.id})) { index in
-                            ZStack {
+                            
                                 NavigationLink(destination: Navigation(postpoke: index)) {
+                                    
+                                    ZStack {
                                     Circle()
                                         .trim(from: 0.5, to: 1.0)
                                         .foregroundColor(Color.color)
                                         .frame(height: 180)
-                                }
-                                    VStack {
-                                       
-                                        //ポケモンの番号
-                                        Text("No.\(index.id)").foregroundColor(.black)
                                         
-                                        if let url = URL(string: index.sprites.frontDefault) {
-                                            AsyncImage(url: url) { phase in
-                                                switch phase {
-                                                case .empty:
-                                                    ProgressView()
-                                                case .success(let image):
-                                                    image
-                                                        .resizable()
-                                                        .scaledToFit()
-                                                        .frame(width: 80, height: 80)
-                                                case .failure:
-                                                    Image(systemName: "photo")
-                                                @unknown default:
-                                                    EmptyView()
+                                        VStack {
+                                           
+                                            //ポケモンの番号
+                                            Text("No.\(index.id)").foregroundColor(.black)
+                                            
+                                            if let url = URL(string: index.sprites.frontDefault) {
+                                                AsyncImage(url: url) { phase in
+                                                    switch phase {
+                                                    case .empty:
+                                                        ProgressView()
+                                                            .frame(width: 80, height: 80)
+
+                                                    case .success(let image):
+                                                        image
+                                                            .resizable()
+                                                            .aspectRatio(contentMode: .fit)
+                                                            .frame(width: 80, height: 80)
+                                                        
+                                                        
+                                                    case .failure:
+                                                        Image(systemName: "photo")
+                                                            .resizable()
+                                                                   .aspectRatio(contentMode: .fit)
+                                                                   .frame(width: 80, height: 80)
+                                                        
+                                                    @unknown default:
+                                                        EmptyView()
+                                                            .frame(width: 80, height: 80)
+
+                                                    }
                                                 }
                                             }
+                                            
+                                            
+                                            //画像にテキストを重ねる方法
+                                            Text("\(index.name)").foregroundColor(.black)
                                         }
                                         
                                         
-                                        //画像にテキストを重ねる方法
-                                        Text("\(index.name)").foregroundColor(.black)
                                     }
+                                    //Circleの外枠の線
+                                    .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                                    
                                 }
+                                
+                               
+                                   
+                               
                             }
-                            //Circleの外枠の線
-                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
+
                         }
                         .padding(.bottom)
+                        
                     
                     }
                     //画面を表示する際に動く
-                
-                    .onAppear(){
-                        pokemonViewModel.fetchPosts()
-                        
-                    }
+            
+//                    .onAppear(){
+//                        pokemonViewModel.fetchPosts()
+//                        
+//                    }
                 }
+            
             }
         }
 }
